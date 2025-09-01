d11ead7aeb13aaf541d158f923bd409b9dc91536
Karthiknaidu9 <95526353+Karthiknaidu9@users.noreply.github.com>
2025-09-01 17:27:53 +0530

    Time: 3 ms (79.70%) | Memory: 45.9 MB (45.22%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,30 +1,26 @@
 class Solution {
     public int candy(int[] ratings) {
-        int[] arr = new int[ratings.length];
-        Arrays.fill(arr, 1);
-        boolean changed = true;
-        while (changed) {
-            changed = false;
-            for (int i = 0; i < arr.length; i++) {
-                int leftval = 0, rightval = 0;
-                if (i != 0 && ratings[i - 1] < ratings[i] &&arr[i - 1] >= arr[i]) {
-                    changed=true;
-                    leftval = arr[i - 1];
-                }
-                if (i != arr.length - 1 && ratings[i + 1] < ratings[i]&& arr[i + 1] >= arr[i]) {
-                    changed=true;
-                    rightval = arr[i + 1];
-                }
-                if(leftval!=0||rightval!=0){
-                    arr[i]=Math.max(leftval,rightval)+1;
-                }
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
             }
         }
-        int res=0;
-        for(int i = 0; i < arr.length; i++){
-        // System.out.println(arr[i]);
-            res+=arr[i];
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
         }
-        return res;
+
+        int totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
+        }
+
+        return totalCandies;
     }
 }
